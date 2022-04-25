3666bb073dfeda7ccabe7db417149eeebd72c656
LucasHemm <99349701+LucasHemm@users.noreply.github.com>
2022-04-25 10:35:05 +0200

    done with loadplayer and loadteam data

--- src/DatabaseIO.java
+++ src/DatabaseIO.java
@@ -1,4 +1,8 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.sql.*;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class DatabaseIO implements IFileIO {
 
@@ -10,8 +14,147 @@ public class DatabaseIO implements IFileIO {
 
     @Override
     public String[] loadTeamData() {
-        return new String[0];
+        Connection connection = null;
+        String JdbcUrl = "jdbc:mysql://localhost/world?" + "autoReconnect=true&useSSL=false";
+        String username = "root";
+        String password = "*****";
+        String[] teamData = new String[8];
+        try {
+            connection = DriverManager.getConnection(JdbcUrl, username, password);
+            PreparedStatement statement = connection.prepareStatement("SELECT * FROM sp3.teams ORDER BY id");
+
+            ResultSet result = statement.executeQuery();
+
+            int counter = 0;
+            while (result.next()) {
+
+                String teamName = result.getString("name");
+                String numOfPlayers = result.getString("numberOfPlayers");
+                String points = result.getString("points");
+                String goalDiff = result.getString("goalDiff");
+
+                String teamBuild = teamName + ", " + numOfPlayers + ", " + points + ", " + goalDiff;
+
+                teamData[counter] = teamBuild;
+
+                counter++;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return teamData;
     }
+
+    @Override
+    public ArrayList<String> loadPlayerData() {
+        Connection connection = null;
+        String JdbcUrl = "jdbc:mysql://localhost/world?" + "autoReconnect=true&useSSL=false";
+        String username = "root";
+        String password = "*****";
+        ArrayList<String> playerData = new ArrayList<>();
+        try {
+                connection = DriverManager.getConnection(JdbcUrl, username, password);
+                PreparedStatement statement1 = connection.prepareStatement("SELECT * FROM sp3.players ORDER BY teamid");
+
+                ResultSet result1 = statement1.executeQuery();
+                String s1 = "";
+                String s2 = "";
+                String s3 = "";
+                String s4 = "";
+                String s5 = "";
+                String s6 = "";
+                String s7 = "";
+                String s8 = "";
+                while (result1.next()) {
+
+                    String playerName = result1.getString("name");
+                    int teamid = result1.getInt("teamid");
+
+                   switch(teamid){
+
+                       case 1:
+
+                           s1 += playerName + ", ";
+                           break;
+                       case 2:
+                           s2 += playerName + ", ";
+
+                           break;
+                       case 3:
+                           s3 += playerName + ", ";
+
+                           break;
+
+                       case 4:
+                           s4 += playerName + ", ";
+
+                           break;
+                       case 5:
+                           s5 += playerName + ", ";
+
+                           break;
+                       case 6:
+                           s6 += playerName + ", ";
+
+                           break;
+                       case 7:
+                           s7 += playerName + ", ";
+
+                           break;
+                       case 8:
+                           s8 += playerName + ", ";
+                           break;
+                   }
+                    playerData.add(s1);
+                    playerData.add(s2);
+                    playerData.add(s3);
+                    playerData.add(s4);
+                    playerData.add(s5);
+                    playerData.add(s6);
+                    playerData.add(s7);
+                    playerData.add(s8);
+                }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return playerData;
+    }
+
+    @Override
+    public ArrayList<String> loadGameData() {
+        Connection connection = null;
+        String JdbcUrl = "jdbc:mysql://localhost/world?" + "autoReconnect=true&useSSL=false";
+        String username = "root";
+        String password = "******";
+        ArrayList<String> gameData = new ArrayList<>();
+        try {
+            connection = DriverManager.getConnection(JdbcUrl, username, password);
+            PreparedStatement statement = connection.prepareStatement("SELECT * FROM sp3.teams ORDER BY id");
+
+            ResultSet result = statement.executeQuery();
+
+            int counter = 0;
+            while (result.next()) {
+
+                String teamName = result.getString("name");
+                String numOfPlayers = result.getString("numberOfPlayers");
+                String points = result.getString("points");
+                String goalDiff = result.getString("goalDiff");
+
+                String teamBuild = teamName + ", " + numOfPlayers + ", " + points + ", " + goalDiff;
+
+                teamData[counter] = teamBuild;
+
+                counter++;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return gameData;
+
+    }
+
+
 
     @Override
     public void saveTeamData(ArrayList<Team> data) {
@@ -23,15 +166,7 @@ public class DatabaseIO implements IFileIO {
 
     }
 
-    @Override
-    public ArrayList<String> loadPlayerData() {
-        return null;
-    }
 
-    @Override
-    public ArrayList<String> loadGameData() {
-        return null;
-    }
 
     @Override
     public void clear() {
